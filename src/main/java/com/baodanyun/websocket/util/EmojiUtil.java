ef25773e6bc6f4b891df38428e4994d47b7d4db7
hubo <bhu@huikedu.com>
2017-07-05 14:14:00 +0800

    tranform emoji html unicode content to string

--- src/main/java/com/baodanyun/websocket/util/EmojiUtil.java
+++ src/main/java/com/baodanyun/websocket/util/EmojiUtil.java
@@ -1,5 +1,7 @@
 package com.baodanyun.websocket.util;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,8 +19,9 @@ public class EmojiUtil {
      * 转换后的内容： 欢迎新会员'表情'
      */
     public static String tranformEemojiContent(String span) {
-        Pattern pattern = Pattern.compile("<span.*?>.*?</span>");
+        span = StringEscapeUtils.unescapeJava(span); //unicode码如\u003cspan
 
+        Pattern pattern = Pattern.compile("<span.*?>.*?</span>");
         Pattern p = Pattern.compile(".*?<span.*?class=.*?emoji emoji(.*?)\"></span>.*?");
         Matcher matcher = p.matcher(span);
         while (matcher.find()) {
@@ -35,10 +38,11 @@ public class EmojiUtil {
         return span.trim();
     }
 
-    /*public static void main(String[] args) {
-        String content = "";//"欢<span class=\"emoji emoji1f75f\"></span>迎新<span class=\"emoji emoji1f44f\"></span>会员<span class=\"emoji emoji1f45f\"></span>天天币<span class=\"emoji emoji1f64f\"></span>";
-        String result = tranformEemojiContent(content);
+    public static void main(String[] args) throws  Exception {
+        String content = "<span class=\"emoji emoji1f604\"></span>天天\\u003cspan class\\u003d\\\"emoji emoji1f60a\\\"\\u003e\\u003c/span\\u003e\\u003cspan class\\u003d\\\"emoji emoji1f63a\\\"\\u003e\\u003c/span\\u003e\\u003cspan class\\u003d\\\"emoji emoji1f604\\\"\\u003e\\u003c/span\\u003e\\u003cspan class\\u003d\\\"emoji emoji1f639\\\"\\u003e\\u003c/span\\u003e\\u003cspan class\\u003d\\\"emoji emoji1f62d\\\"\\u003e\\u003c/span\\u003e\"";//"欢<span class=\"emoji emoji1f75f\"></span>迎新<span class=\"emoji emoji1f44f\"></span>会员<span class=\"emoji emoji1f45f\"></span>天天币<span class=\"emoji emoji1f64f\"></span>";
 
-        System.out.println(result);
-    }*/
+       content =  StringEscapeUtils.unescapeJava(content);
+
+        System.out.println(tranformEemojiContent(content));
+    }
 }
