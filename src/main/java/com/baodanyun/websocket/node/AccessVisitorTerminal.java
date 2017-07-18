83a7fba2cdf8fb2bebcdcc261b9f7763b18fb978
liaowuhen <296558063@qq.com>
2017-07-18 18:05:39 +0800

    合并胡波分支

--- src/main/java/com/baodanyun/websocket/node/AccessVisitorTerminal.java
+++ src/main/java/com/baodanyun/websocket/node/AccessVisitorTerminal.java
@@ -1,7 +1,6 @@
 package com.baodanyun.websocket.node;
 
 import com.baodanyun.websocket.bean.msg.Msg;
-import com.baodanyun.websocket.bean.user.AbstractUser;
 import com.baodanyun.websocket.event.VisitorLoginEvent;
 import com.baodanyun.websocket.event.VisitorReciveMsgEvent;
 import com.baodanyun.websocket.exception.BusinessException;
@@ -43,12 +42,6 @@ public class AccessVisitorTerminal extends WeChatTerminal {
                 this.getChatNodeAdaptation().getAbstractUser(), CommonConfig.LOGIN__FROM_WE_CHAT_PASSIVE);
         EventBusUtils.post(vle);
     }
-
-    @Override
-    boolean joinQueue(AbstractUser customer) {
-        return false;
-    }
-
 
     @Override
     boolean customerOnline() {
